47c69ad202f381f5fdfb3d150847a68c5ba189c7
gabrielheiwa <heiwa.gabriel@gmail.com>
2021-02-26 09:58:44 -0300

    make modification in the whatsapp cliente

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,16 @@
 import express from "express";
+import { createServer } from "http";
 import path from "path";
-import { createServer, ServerOptions } from "http";
 import { Server } from "socket.io";
 import { router } from "./routes";
-import { SocketOptions } from "dgram";
-import { ServerOpts } from "net";
-
+import morgan from "morgan";
 
 const app = express();
 const server = createServer(app);
 const PORT = process.env.PORT || 3000;
 
 
+app.use(morgan("dev"));
 app.use(express.static(path.resolve(__dirname, "public")));
 app.use(router)
 
--- src/routes.ts
+++ src/routes.ts
@@ -1,27 +1,36 @@
 import { Router } from "express";
 import { Client } from "whatsapp-web.js";
 import { create, toDataURL } from "qrcode";
+import { socketServer } from ".";
 
 
 const router = Router();
 
 
 router.get("/qrcode", (request, response) => {
-    const ClientWhatsapp = new Client({});
+    const ClientWhatsapp = new Client({
+        puppeteer: {
+            args: [
+                '--no-sandbox',
+                '--disable-setuid-sandbox',
+            ],
+        }
+    });
     const qrcode = require("qrcode-terminal");
 
 
     ClientWhatsapp.on("qr", async (qr) => {
         const imgSrc = await toDataURL(qr, {})
-        response.send(`<img src="${imgSrc}"/>`);
+        socketServer.emit("qr", imgSrc);
     });
-
+    
     ClientWhatsapp.on("ready", () => {
         console.log("Whathsapp conectado!");
     });
 
-
     ClientWhatsapp.initialize();
+
+    return response.end()
 });
 
 export { router };
